82e75d814bba1b772fc6ba54a0d3f39018cda9a0
Jose Ricardo Ziviani <jose@ziviani.net>
2017-11-01 12:53:04 -0200

    Fix partition
    Signed-off-by: Jose Ricardo Ziviani <jose@ziviani.net>

--- sorting/quicksort.h
+++ sorting/quicksort.h
@@ -11,6 +11,7 @@ namespace stuff
 {
     inline size_t get_pivot(size_t left, size_t right)
     {
+        return left;
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<size_t> dis(left, right - 1);
@@ -24,22 +25,16 @@ namespace stuff
                      std::function<bool(const T &a, const T &b)> fn)
     {
         std::swap(c[get_pivot(left, right)], c[left]);
-        size_t pivot = left++;
+        size_t pivot = left;
+        size_t pi = left + 1;
 
-        while (left < right)
-        {
-            if (fn(c[left], c[pivot]))
-                left++;
-            else
-                std::swap(c[left], c[right--]);
+        for (size_t i = pi; i <= right; i++) {
+            if (fn(c[i], c[pivot]))
+                std::swap(c[i], c[pi++]);
         }
+        std::swap(c[pivot], c[--pi]);
 
-        if (fn(c[pivot], c[left]))
-            left--;
-
-        std::swap(c[pivot], c[left]);
-
-        return left;
+        return pi;
     }
 
     template <class C, typename T>
@@ -48,11 +43,12 @@ namespace stuff
                    size_t right,
                    std::function<bool(const T &a, const T &b)> fn)
     {
-        if (left >= right)
+        if (left > right)
             return;
 
         size_t pivot = partition(c, left, right, fn);
-        quicksort(c, left, pivot, fn);
+        if (pivot > 0)
+            quicksort(c, left, pivot - 1, fn);
         quicksort(c, pivot + 1, right, fn);
     }
 
