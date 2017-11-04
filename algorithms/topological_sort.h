4ce1bbacfb80d221f0e8d8f5ea0726ac17161c38
Jose Ricardo Ziviani <jose@ziviani.net>
2017-11-04 13:40:47 -0200

    Add topo sort
    Signed-off-by: Jose Ricardo Ziviani <jose@ziviani.net>

---
+++ algorithms/topological_sort.h
@@ -0,0 +1,53 @@
+#ifndef TOPOLOGICAL_SORT_H_
+#define TOPOLOGICAL_SORT_H_
+
+#include "digraph.h"
+#include "stack.h"
+#include "vector.h"
+
+#include <functional>
+#include <iostream>
+
+namespace stuff
+{
+    class topological_sort
+    {
+        vector<bool> visited_;
+        vector_stack<int> reverse_;
+
+        public:
+        topological_sort(digraph &g) :
+            visited_(g.vertices(), false)
+        {
+            for (size_t v = 0; v < g.vertices(); v++) {
+                if (visited_[v])
+                    continue;
+
+                dfs(g, v);
+            }
+        }
+
+        void dfs(digraph &g, int v)
+        {
+            visited_[v] = true;
+
+            const auto visit = [this, &g](const int &w) {
+                if (visited_[w])
+                    return;
+
+                dfs(g, w);
+            };
+            g.adj(v).iterate(visit);
+            reverse_.push(v);
+        }
+
+        void to_string()
+        {
+            while(!reverse_.is_empty()) {
+                std::cout << reverse_.pop() << '\n';
+            }
+        }
+    };
+}
+
+#endif
